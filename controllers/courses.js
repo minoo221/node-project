e7f0b458fe578f531f8b70cd8eef35d1ed6f48f8
Milan Blasko <milan.blasko@interway.sk>
2020-11-10 10:52:09 +0100

    refactor controllers

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,5 +1,5 @@
-const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
 
 const Course = require("../models/Course");
 const Bootcamp = require("../models/Bootcamp");
@@ -9,24 +9,19 @@ const Bootcamp = require("../models/Bootcamp");
 // @route     GET /api/v1/bootcamps/:bootcampsId/courses
 // @access    Public
 exports.getCourses = asyncHandler( async(req, res, next) => {
-	let query;
 
 	if (req.params.bootcampId) {
-		query = Course.find({ bootcamp: req.params.bootcampId })
+		const courses = await Course.find({ bootcamp: req.params.bootcampId })
+
+		return res.status(200),json({
+			success: true,
+			count: courses.length,
+			data: courses
+		})
 	} else {
-		query = Course.find().populate({
-			path: 'bootcamp',
-			select: 'name description'
-		});
+		res.status(200).json(res.advancedResults);
 	}
 
-	const courses = await query;
-
-	res.status(200).json({
-		success: true,
-		count: courses.length,
-		data: courses
-	})
 })
 
 // @desc      Get course
@@ -65,5 +60,44 @@ exports.addCourse = asyncHandler( async(req, res, next) => {
 	res.status(200).json({
 		success: true,
 		data: course
+	})
+})
+
+// @desc      Update course
+// @route     PUT /api/v1/courses/:id
+// @access    Private
+exports.updateCourse = asyncHandler( async(req, res, next) => {
+	let course = await Course.findById(req.params.id);
+
+	if (!course) {
+		return next(new ErrorResponse(`No course with the id ${req.params.id}`), 404)
+	}
+
+	course = await Course.findByIdAndUpdate(req.param.id, req.body, {
+		new: true,
+		runValidators: true
+	})
+
+	res.status(200).json({
+		success: true,
+		data: course
+	})
+})
+
+// @desc      Delete course
+// @route     DETELE /api/v1/courses/:id
+// @access    Private
+exports.deleteCourse = asyncHandler( async(req, res, next) => {
+	const course = await Course.findById(req.params.id);
+
+	if (!course) {
+		return next(new ErrorResponse(`No course with the id ${req.params.id}`), 404)
+	}
+
+	await course.remove()
+
+	res.status(200).json({
+		success: true,
+		data: {}
 	})
 })
--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,38 @@ const CourseSchema = new mongoose.Schema({
 	}
 });
 
+// Static method to get avg of course tuitions
+CourseSchema.static.getAverageCost = async function() {
+
+	const obj = await this.aggregate([
+		{
+			$match: {bootcamp: bootcampId}
+		},
+		{
+			$group: {
+				_id: '$bootcamp',
+				averageCost: {$avg: '$tuition'}
+			}
+		}
+	])
+
+	try {
+		await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+			averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+		})
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+// Call getAverageCost after save
+CourseSchema.post('save', function() {
+	this.constructor.getAverageCost(this.bootcamp)
+})
+
+// Call getAverageCost before save
+CourseSchema.pre('remove', function() {
+	this.constructor.getAverageCost(this.bootcamp);
+})
+
 module.exports = mongoose.model('Course', CourseSchema)
